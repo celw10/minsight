5b933d5bad0e72f1c7a84421d1ae12f65dcd4083
celw10 <34209214+celw10@users.noreply.github.com>
2023-04-29 13:25:46 -0230

    ArcGIS Maps SDK for JS working example

--- src/assets/gis.tsx
+++ src/assets/gis.tsx
@@ -1,33 +1,22 @@
-// import React, { useState } from 'react';
-// import '@esri/calcite-components/dist/components/calcite-button';
-// import '@esri/calcite-components/dist/components/calcite-icon';
-// import '@esri/calcite-components/dist/components/calcite-slider';
-// import {
-//   CalciteButton,
-//   CalciteIcon,
-//   CalciteSlider
-// } from '@esri/calcite-components-react';
-// import '@esri/calcite-components/dist/calcite/calcite.css';
+import React, { useState } from 'react';
+import WebMap from "@arcgis/core/WebMap.js";
 
-// export const Test = () => {
-//   const [sliderValue, setSliderValue] = useState(50);
+/************************************************************
+ * Creates a new WebMap instance. A WebMap must reference
+ * a PortalItem ID that represents a WebMap saved to
+ * arcgis.com or an on-premise portal.
+ ************************************************************/
 
-//   return (
-//     <>
-//       <h1>
-//         Hello, React <CalciteIcon icon="banana" />
-//       </h1>
-//       <CalciteButton onClick={(e: any) => setSliderValue(0)}>Reset</CalciteButton>
-//       <CalciteSlider
-//         min="1"
-//         max="100"
-//         value={sliderValue}
-//         step="1"
-//         onCalciteSliderInput={(e: any) => setSliderValue(e.target.value)}
-//       />
-//       <p>The slider currently has a value of {sliderValue}</p>
-//     </>
-//   );
-// }
+const webmap: any = new WebMap({
+  portalItem: {
+    id: "e691172598f04ea8881cd2a4adaa45ba"
+  }
+});
 
-
+/************************************************************
+ * Set the WebMap instance to the map property in a MapView.
+ ************************************************************/
+const view: any = new MapView({
+  map: webmap,
+  container: "viewDiv" //viewDiv was simply in my HTML last time, Can I have this div in App?
+});
